239fdb514e678eea6ac09b5ef582f13b455163d9
Gabor Greif <gabor@dfinity.org>
2026-04-12 17:56:08 +0200

    chore: bump `clang` to v20

--- nix/rts.nix
+++ nix/rts.nix
@@ -56,8 +56,8 @@ pkgs.stdenv.mkDerivation {
   nativeBuildInputs = [ pkgs.makeWrapper pkgs.removeReferencesTo pkgs.cacert ];
 
   buildInputs = with pkgs; [
-    llvmPackages_19.clang
-    llvmPackages_19.bintools
+    llvmPackages_20.clang
+    llvmPackages_20.bintools
     rust-nightly
     wasmtime
     rust-bindgen
